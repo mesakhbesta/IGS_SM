ef2150a853f411e71cb5f21f0bee708c9386d245
mesakhbesta <mesakhbesta@gmail.com>
2025-01-28 20:25:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def create_driver():
     from selenium.webdriver.firefox.options import Options
 
     options = Options()
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
